f0228ad9cb5c264db8293270c6e66bb3d71860ff
Remco75 <rcvlierman@gmail.com>
2020-07-24 12:16:29 +0200

    5 files modified

--- src/app/formsteps/steptwo/steptwo.component.ts
+++ src/app/formsteps/steptwo/steptwo.component.ts
@@ -6,29 +6,22 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   templateUrl: './steptwo.component.html',
   styleUrls: ['./steptwo.component.css']
 })
-export class SteptwoComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
+export class SteptwoComponent implements OnInit {
  public stepform: FormGroup;
   @Output() done: EventEmitter<any> = new EventEmitter();
+  @Output() somethingChangedSoResetLaterSteps = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
-    console.log('init!')
     this.stepform = new FormGroup({
       reason: new FormControl([''])
     });
   }
-
-  ngOnChanges() {
-    console.log('change')
-  }
-
-  ngAfterViewInit() {
-    console.log('viewInit')
-  }
-  ngOnDestroy() {
-    console.log('destroy')
-  }
   onReasonDone() {
     this.done.emit();
+  }
+
+  resetLaterStep() {
+    
   }
 }
--- src/app/scroll-wizard/scroll-wizard-step.directive.ts
+++ src/app/scroll-wizard/scroll-wizard-step.directive.ts
@@ -1,34 +1,35 @@
-import { Directive, Input, ViewRef, TemplateRef, ViewContainerRef, OnInit, OnChanges, HostBinding } from "@angular/core";
+import {Directive,ViewRef,TemplateRef,ViewContainerRef,OnInit} from "@angular/core";
 import { WizardService } from "./wizard.service";
 
 @Directive({
   selector: "[appScrollWizardStep]"
 })
 export class ScrollWizardStepDirective implements OnInit {
-   
   private hasView = false;
   public active: boolean;
   public stepIndex: number;
   private stepViewRef: ViewRef;
-  
-  constructor(private wizardService: WizardService, 
-  private templateRef: TemplateRef<any>,
-  private viewContainer: ViewContainerRef) {}
-  @HostBinding('class.scroll-step') scrollStel = true;
-  onNavigationStreamChange(navEvent) {
-    // this is the API for our steps. Methods / properties on this context can be called from the implementing template
-    const context = {
-      stepCtrl: {
-        resetFromHere: (stepIndex = this.stepIndex) => this.wizardService.resetFromStep(stepIndex),
-        goNext: () => this.wizardService.gotoNextStep()
-      },
-      index: this.stepIndex
-    };
 
+  constructor(private wizardService: WizardService, private templateRef: TemplateRef<any>,private viewContainer: ViewContainerRef) {}
+
+  ngOnInit() {
+    this.wizardService.navigation$.subscribe(navEvent =>this.onNavigationStreamChange(navEvent));
+    this.wizardService.removeStep$.subscribe(index => this.onRemove(index));
+  }
+
+  onNavigationStreamChange(navEvent) {
     if (this.active) {
-        this.stepViewRef = this.viewContainer.createEmbeddedView(this.templateRef, context);
-        this.hasView = true;
-        this.viewContainer.insert(this.stepViewRef);
+      // this is the API for our steps. Methods / properties on this context can be called from the implementing template
+      const context = {
+        stepCtrl: {
+          resetFromHere: (stepIndex = this.stepIndex) => this.wizardService.resetFromStep(stepIndex),
+          goNext: () => this.wizardService.gotoNextStep()
+        },
+        index: this.stepIndex
+      };
+      this.stepViewRef = this.viewContainer.createEmbeddedView(this.templateRef, context);
+      this.hasView = true;
+      this.viewContainer.insert(this.stepViewRef);
     }
   }
 
@@ -37,10 +38,5 @@ export class ScrollWizardStepDirective implements OnInit {
       this.hasView = false;
       this.viewContainer.clear();
     }
-  }
-
-  ngOnInit() {
-    this.wizardService.navigation$.subscribe(navEvent => this.onNavigationStreamChange(navEvent));
-    this.wizardService.removeStep$.subscribe(index => this.onRemove(index));
   }
 }
